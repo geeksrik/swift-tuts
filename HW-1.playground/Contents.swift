ab65b0ee4266d9576daed926dd753bb86e1b38bd
geeksrik <geeksrik@gmail.com>
2019-02-18 19:51:26 +0530

    homework-1
    homework-1

---
+++ HW-1.playground/Contents.swift
@@ -0,0 +1,10 @@
+// Homework #1 Get Swifty part 1
+//Information on the song "faded"
+let songname: String = "Faded"
+let artist: String = "Alan Walker"
+let date_released: (Int,String,Int) = (3,"Dec",2015)
+let songwriters: (String,String,String,String) = ("Alan Walker","Jesper Borgen","Anders Frøen","Gunnar Greve Pettersen")
+let label: String = "Mer Musikk"
+let genres: (String,String,String,String) = ("Electronica","House music","Progressive house","Dance/Electronic")
+let song_length : Float = 3.32  //3 minutes & 32 seconds ,not 3.32 minutes
+let isCopyrighted : Bool = false
